96959f7b8ee44dae1b40c766d7eb806939647bab
avihenri <avril.m.henry@gmail.com>
2023-08-01 20:06:39 +0100

    services improvements

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -7,10 +7,12 @@ function Services () {
         <PawTitle title='our services' classnames='2xl:pb-10' titleClassnames='text-brand-eggShell py-4 px-2' />
 
         <div className="w-full mb-8 p-2">
-            <h6 className="text-brand-sand text-center w-11/12 m-auto sm:w-full text-xl sm:text-2xl lg:text-3xl 2xl:text-6xl pb-1 my-6"><span className='uppercase font-bold font-gagalin'>Group Walks</span> <br /><span className='text-brand-eggShell text-lg lg:text-xl 2xl:text-4xl'>Socialize and Exercise Together</span></h6>
-            <h6 className="text-brand-sand text-center w-11/12 m-auto sm:w-full text-xl sm:text-2xl lg:text-3xl 2xl:text-6xl pb-1 my-6"><span className='uppercase font-bold'>Solo Walks</span> <br /><span className='text-brand-eggShell sm:text-xl text-lg lg:text-xl 2xl:text-4xl'>Tailored Care for Your Companion</span></h6>
-            <h6 className="text-brand-sand text-center w-11/12 m-auto sm:w-full text-xl sm:text-2xl lg:text-3xl 2xl:text-6xl pb-1 my-6"><span className='uppercase font-bold'>Puppy Visits</span> <br /><span className='text-brand-eggShell text-lg lg:text-xl 2xl:text-4xl'>Caring for Your Pups While You're Away</span></h6>
-            <p className="text-brand-secondary text-center sm:w-9/12 lg:w-8/12 m-auto my-6 text-sm sm:text-xl lg:text-2xl 2xl:text-5xl py-4">Before we go on any walks or do any visits with your pup, we can organise a meet &amp; greet with yourself so we can get to know each other first.</p>
+            <div className='h-1 w-1/2 md:w-3/12 bg-brand-secondary m-auto my-7 rounded-lg'></div>
+            <h6 className="text-brand-sand text-center w-11/12 m-auto sm:w-full text-xl sm:text-2xl lg:text-3xl 2xl:text-6xl pb-1 my-6"><span className='uppercase font-bold font-mono'>Group Walks</span> <br /><span className='text-brand-eggShell text-lg lg:text-xl 2xl:text-4xl'>Socialize and Exercise Together</span></h6>
+            <h6 className="text-brand-sand text-center w-11/12 m-auto sm:w-full text-xl sm:text-2xl lg:text-3xl 2xl:text-6xl pb-1 my-6"><span className='uppercase font-bold font-mono'>Solo Walks</span> <br /><span className='text-brand-eggShell sm:text-xl text-lg lg:text-xl 2xl:text-4xl'>Tailored Care for Your Companion</span></h6>
+            <h6 className="text-brand-sand text-center w-11/12 m-auto sm:w-full text-xl sm:text-2xl lg:text-3xl 2xl:text-6xl pb-1 my-6"><span className='uppercase font-bold font-mono'>Puppy Visits</span> <br /><span className='text-brand-eggShell text-lg lg:text-xl 2xl:text-4xl'>Caring for Your Pups While You're Away</span></h6>
+            <div className='h-1 w-1/3 md:w-2/12 bg-brand-secondary m-auto my-7 rounded-lg'></div>
+            <p className="text-brand-eggShell text-center sm:w-9/12 lg:w-8/12 m-auto my-6 text-sm sm:text-xl lg:text-2xl 2xl:text-5xl py-4">Before we go on any walks or do any visits with your pup, we can organise a meet &amp; greet with yourself so we can get to know each other first.</p>
             <img className='w-11/12 m-auto sm:w-1/2 lg:w-1/3 2xl:w-1/2 shadow-2xl my-8 lg:mt-8 2xl:mt-10 border-8 border-brand-secondary' src='/price-list.png' />
             <p className="text-brand-secondary text-center sm:w-7/12 m-auto text-sm lg:text-lg 2xl:text-3xl my-5 py-4">*Prices are subject to change. <br />*We may need to do Solo Walks if no other clients in your area.</p>
         </div>
