9409c29522d09804394199a934a2eeb3979eefcb
avihenri <avril.m.henry@gmail.com>
2023-08-10 18:00:53 +0100

    allow indexing with meta tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,11 +6,13 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Paw Prints Perth | Professional Dog Walking Services in Scone, Perth UK',
   description: 'Looking for reliable dog walking services in Scone, Perth and surrounding areas? Paw Prints Perth offers group walks, solo walks, and pup visits for your furry friends. Trust Paw Prints Perth to be your dog&apos;s best friend when you can&apos;t be there. Contact us today!',
+
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta name="robots" content="index, follow" />
       <body className={inter.className}>{children}</body>
     </html>
   )
